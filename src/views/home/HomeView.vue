5230999f8fd91b05d5ffe3f4aab0e3f394bcfa50
Heesu01 <rulru0413@naver.com>
2025-11-18 15:00:00 +0900

    chore: 기본 페이지 구조 및 라우터 뼈대 설정

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-xl font-bold">홈화면</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/mypage/MyPageHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-xl font-bold">마이페이지</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
